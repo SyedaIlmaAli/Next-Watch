cd6cca5c1f97e09ae0ba0b95aa8f8f95e002a01d
SyedaIlmaAli <syedailmazaidi5@gmail.com>
2024-11-20 19:22:44 +0500

    styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,32 @@
-import Link from 'next/link'
-import React from 'react'
-import { FaShoppingCart } from 'react-icons/fa'
+"use client"
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className='header'>
-      <h1 className='logo'>Next Watch</h1>
-      <nav>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/about"}>About</Link>
-        <Link href={"/watches"}>Watches</Link>
-        <Link href={"/contact"}>Contact</Link>
-        <FaShoppingCart style={{ color: 'white', fontSize: '20px', cursor: "pointer" }} />
+    <header className="header">
+      <h1 className="logo">Next Watch</h1>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        &#9776;
+      </div>
+      <nav className={menuOpen ? 'open' : ''}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/watches">Watches</Link>
+        <Link href="/contact">Contact</Link>
+        <FaShoppingCart
+          style={{ color: 'white', fontSize: '20px', cursor: 'pointer' }}
+        />
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
